774cb18b86db0d12c3adad2942543ee6bb0aae88
Luis Miranda <luismiranda@MacBook-Pro-de-Luis.local>
2023-06-01 21:55:22 -0500

    Include info in the about page

--- src/components/pokemon-info/info-about/index.tsx
+++ src/components/pokemon-info/info-about/index.tsx
@@ -1,10 +1,13 @@
 import { useSelector } from 'react-redux';
 import { RooState } from '../../../store';
+import InfoAboutStyles from './info-about.style';
 
 const InfoAbout = () => {
   const pokemonContent = useSelector(
     (state: RooState) => state.selectedPokemon.pokemonContent
   );
+  const { CapitalizeLabel, Section, SectionContent, SectionHeader } =
+    InfoAboutStyles;
 
   return (
     <div>
@@ -14,6 +17,36 @@ const InfoAbout = () => {
           {pokemonContent?.description}
         </p>
       </div>
+      <Section>
+        <SectionHeader $typeColor={pokemonContent?.color ?? ''}>
+          Pokedex Data
+        </SectionHeader>
+        <SectionContent>
+          <div>Specie:</div>
+          <CapitalizeLabel>{pokemonContent?.nameSpecie}</CapitalizeLabel>
+          <div>Height:</div>
+          <div>{pokemonContent?.height}</div>
+          <div>Weight:</div>
+          <div>{pokemonContent?.weight}</div>
+          <div>Habitat:</div>
+          <CapitalizeLabel>{pokemonContent?.habitat}</CapitalizeLabel>
+        </SectionContent>
+      </Section>
+      <Section>
+        <SectionHeader $typeColor={pokemonContent?.color ?? ''}>
+          Training
+        </SectionHeader>
+        <SectionContent>
+          <div>Catch Rate:</div>
+          <div>{pokemonContent?.captureRatio}</div>
+          <div>Base Friendship:</div>
+          <div>{pokemonContent?.baseHappiness}</div>
+          <div>Base Exp:</div>
+          <div>{pokemonContent?.baseExperience}</div>
+          <div>Growth Rate:</div>
+          <CapitalizeLabel>{pokemonContent?.growthRate}</CapitalizeLabel>
+        </SectionContent>
+      </Section>
     </div>
   );
 };
---
+++ src/components/pokemon-info/info-about/info-about.style.ts
@@ -0,0 +1,35 @@
+import { styled } from 'styled-components';
+
+interface ISectionHeader {
+  $typeColor: string;
+}
+
+const Section = styled.div`
+  margin: 12px 0 12px;
+`;
+
+const SectionHeader = styled.div<ISectionHeader>`
+  color: ${({ $typeColor }) => $typeColor};
+  border-bottom: ${({ $typeColor }) => `1px solid ${$typeColor}`};
+  font-weight: 600;
+`;
+
+const SectionContent = styled.div`
+  display: grid;
+  grid-template-columns: 50% 50%;
+  padding: 12px 0 12px;
+  row-gap: 6px;
+`;
+
+const CapitalizeLabel = styled.div`
+  text-transform: capitalize;
+`;
+
+const InfoAboutStyles = {
+  CapitalizeLabel,
+  Section,
+  SectionContent,
+  SectionHeader,
+};
+
+export default InfoAboutStyles;
